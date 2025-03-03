26252d72a45087cd2cc6ac18aa2bb4b54bb2b14c
Fabio Vitharana <87120549+FabzHub17@users.noreply.github.com>
2025-03-03 21:19:41 +0100

    bonus part completed

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,8 +1,9 @@
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <stdlib.h>
 # include <unistd.h>
+# include <limits.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 10000000
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,82 @@
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	total_len;
+	char	*joined_str;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	total_len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	joined_str = malloc(total_len * sizeof(char));
+	if (!joined_str)
+		return (NULL);
+	while (s1[i])
+		joined_str[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		joined_str[j++] = s2[i++];
+	joined_str[j] = '\0';
+	return (joined_str);
+}
+
+char	*join_and_free(char *stash, char *buffer)
+{
+	char	*result;
+
+	// Handle case where stash is NULL (first call)
+	if(!stash)
+		return (ft_strdup(buffer));
+	
+	result = ft_strjoin(stash, buffer);
+	free(stash);
+	return (result);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*ptr;
+	size_t	i;
+
+	i = 0;
+	ptr = malloc((ft_strlen(s) + 1) * sizeof(char));
+	if (!ptr)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		ptr[i] = s[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	*ptr;
+
+	ptr = (char *)s;
+	while (*ptr)
+	{
+		if (*ptr == (char)c)
+			return (ptr);
+		ptr++;
+	}
+	if ((char)c == '\0')
+		return (ptr);
+	return (NULL);
+}
